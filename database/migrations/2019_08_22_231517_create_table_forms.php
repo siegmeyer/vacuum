04845bb2f859e9234bcf1bc3efa8f3b40bccfab1
Greg Rothwell <grothwell13@gmail.com>
2019-09-05 19:59:11 -0400

    chore:start facelift

---
+++ database/migrations/2019_08_22_231517_create_table_forms.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableForms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->engine = "InnoDB";
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name');
+            $table->boolean('active')->default(true);
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div id="page" class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,9 +15,39 @@
                     @endif
 
                     You are logged in!
+
+                    Site Checklist
+                    <ul>
+                        <li>Try not to use Bootstrap or jQuery</li>
+                        <li>Use Vue.js and less|sass</li>
+                        <li>Forms</li>
+                        <li>Dynamic Fields</li>
+                        <li>Respondants</li>
+                        <li>
+                            Queuing Jobs
+                            <ul>
+                                <li>Imports</li>
+                                <li>Emails</li>
+                            </ul>
+                        </li>
+                        <li>Queue Management</li>
+                    </ul>
+
+                </div>
+
+            </div>
+            <br >
+            <div class="card">
+                <div class="card-header">Here goes...</div>
+                <div class="card-body">
+                    <a>Create Form</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    console.log(app);
+</script>
 @endsection
